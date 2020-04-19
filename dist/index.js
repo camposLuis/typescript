24a8da4d1c64ce8321f9a9c19b88c6669a5f2ad5
camposLuis <luis.campos.henrique@hotmail.com>
2020-04-18 23:23:28 -0300

    when adding types

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var methods_1 = require("./methods");
 var app = express_1.default();
-app.get('/', function (request, response) {
-    return response.json({ message: 'Hello World' });
-});
+app.get('/', methods_1.helloWorld);
 app.listen(3333);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+import { helloWorld } from './methods';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+app.get('/', helloWorld);
 
 app.listen(3333);
